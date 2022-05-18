c733441737b6be4b1e30804e03816ac56a8455c5
Jose <jmgarcia0202@gmail.com>
2022-05-17 19:13:09 -0700

    redid

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const result = drivers.filter(driversName => driversName.toLowerCase() === name.toLowerCase())
+    return result
+}
+function fuzzyMatch(drivers, name){
+    const result = drivers.filter(driversName => driversName[0] === name[0])
+    return result
+}
+function matchName(drivers, name){
+    return drivers.filter((driversName) => {
+        return driversName.name === name
+    })
+}
+
+/*
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(drivers, string){
@@ -15,3 +30,4 @@ function matchName(drivers, string){
     })
   
 }
+*/
